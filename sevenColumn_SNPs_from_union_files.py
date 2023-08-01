2aeb7cd7d9aed387e779aa02e6ed72738a3b35c3
Darlene Darlington Wagner <darlingtonwagner@gmail.com>
2023-08-01 12:02:25 -0400

    Add files via upload

--- sevenColumn_SNPs_from_union_files.py
+++ sevenColumn_SNPs_from_union_files.py
@@ -7,10 +7,23 @@ from collections import defaultdict
 ## The seven columns should have the following headers:
 ## CHROM   POS     REF     ALT     QUAL    INFO-DP CONSENS
 
+## Note that the script does not invoke argparse
+
+outputSingleCol = True
+outputFull = False
+
 ## Function: Union 
 
 def Union(list1, list2):
     return list(set().union(list1, list2))
+
+## Function: Complement
+
+def Compl(union1, list1):
+    snpSet1 = set(union1)
+    snpSet2 = set(list1)
+    return list(snpSet1 - snpSet2)
+
 
 ## Simple filename extractor function
 
@@ -48,22 +61,38 @@ for argument in sys.argv[1:]:
 
 
 myList = []
+myLines = dict()
 unionList = []
+
 
 #for keys, values in Lines.items():
 #    values['Position']
 
 i = 0
 while i < len(samples):
+    compareArr = unionList.copy()
     for keys1,values1 in SampleSheet[samples[i]].items():
         myList.append(values1['Position'])
     unionList = Union(unionList, myList)
-    #print(len(unionList))
+    complement = Compl(unionList, compareArr)
+    t = 0
+    for item in complement:
+        for keys2,values2 in SampleSheet[samples[i]].items(): #myLines[item] = item
+            if(int(item) == int(values2['Position'])):
+                myLines[item] = {'Ref_ID' : values2['Ref_ID'], 'Position' : values2['Position'], 'Ref' : values2['Ref'], 'Alt' : values2['Alt'], 'Qual' : values2['Qual'], 'Coverage' : values2['Coverage'], 'Fraction' : values2['Fraction']}
+#        t = t + 1
     myList = []
     i = i + 1
 
-print("Union of SNPs positions size is:  ", len(unionList))
-        
-    #for keys, values in SampleSheet[x].items():
-    #    print(values)
+## A Lazy print statement, sys.stderr
+print("Union of SNPs positions size is:  ", len(unionList), file=sys.stderr)
+
+if(outputFull):
+    for keys, values in myLines.items():
+        print(values['Ref_ID'], "\t", values['Position'], "\t", values['Ref'], "\t", values['Alt'],  "\t", values['Qual'], "\t", values['Coverage'], "\t", values['Fraction'])
+else:
+    for keys, values in myLines.items():
+        print(values['Position'])
+
+
 
